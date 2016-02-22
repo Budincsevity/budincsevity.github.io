3df6a16a4786a9c920ac5ed9851b13ec35bec6e6
Norbert Budincsevity <nbudincsevity@gmail.com>
2016-02-22 19:30:52 +0100

    Add imdb page

---
+++ imdb/imdb.js
@@ -0,0 +1,24 @@
+var json = {
+  "Title": "The Shawshank Redemption",
+  "Year": "1994",
+  "Rated": "R",
+  "Released": "14 Oct 1994",
+  "Runtime": "142 min",
+  "Genre": "Crime, Drama",
+  "Director": "Frank Darabont",
+  "Writer": "Stephen King (short story \"Rita Hayworth and Shawshank Redemption\"), Frank Darabont (screenplay)",
+  "Actors": "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
+  "Plot": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+  "Language": "English",
+  "Country": "USA",
+  "Awards": "Nominated for 7 Oscars. Another 14 wins & 20 nominations.",
+  "Poster": "http://ia.media-imdb.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX300.jpg",
+  "Metascore": "80",
+  "imdbRating": "9.3",
+  "imdbVotes": "1,610,411",
+  "imdbID": "tt0111161",
+  "Type": "movie",
+  "Response": "True"
+}
+
+console.log(json)
